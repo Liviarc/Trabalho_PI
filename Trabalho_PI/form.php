5802cb4ed12104a3c9c32b38390d261621c1beb5
Liviarc <78860672+Liviarc@users.noreply.github.com>
2022-11-18 14:24:44 -0300

    Add files via upload

---
+++ Trabalho_PI/form.php
@@ -0,0 +1,41 @@
+<?php
+
+$login = $_POST['login'];
+$email = $_email['email'];
+$senha = MD5($_POST['senha']);
+$nome_do_pet = $_nome_do_pet['nome_do_Pet'];
+$dsn = 'mysql:host=localhost';
+$dbname='dog_ride';
+$query_select = "SELECT login FROM usuarios WHERE login = '$login'";
+$select = 'mysql_query:usuarios';
+$logarray = $array['login'];
+
+  if($login == "" || $login == null){
+    echo"<script language='javascript' type='text/javascript'>
+    alert('O campo login deve ser preenchido');window.location.href='
+    cadastro.html';</script>";
+
+    }else{
+      if($logarray == $login){
+
+        echo"<script language='javascript' type='text/javascript'>
+        alert('Esse login já existe');window.location.href='
+        cadastro.html';</script>";
+        die();
+
+      }else{
+        $query = "INSERT INTO usuarios (login,senha) VALUES ('$login','$senha')";
+        $insert = 'mysql_query:$usuarios';
+
+        if($insert){
+          echo"<script language='javascript' type='text/javascript'>
+          alert('Usuário cadastrado com sucesso!');window.location.
+          href='login.html'</script>";
+        }else{
+          echo"<script language='javascript' type='text/javascript'>
+          alert('Não foi possível cadastrar esse usuário');window.location
+          .href='cadastro.html'</script>";
+        }
+      }
+    }
+?>
